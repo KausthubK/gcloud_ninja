434e0404025d03e353e321cb9a4b8bf1760432e3
Kausthub Krishnamurthy <kausthubk@live.com>
2021-08-06 10:34:40 +1000

    works

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # gcloud_ninja
 Ninja tools to speed up my day to day life with gcloud
+
+unix/macOS only
+
+## install
+
+### Clone
+```
+mkdir .ninja_tools && cd .ninja_tools
+git clone git@github.com:KausthubK/gcloud_ninja.git
+```
+## Modify ~/.zshrc
+Open up ```~/.zshrc``` in any editor of your choice
+
+Add this line: 
+    
+    source ~/.ninja_tools/gcloud_ninja/gci.sh
+
+Or add this line if you want to defensively check for it first:
+    
+    filename=${HOME}/.ninja_tools/gcloud_ninja/gci.sh && if test -f $filename; then source $filename; else "nope - gcloud ninja doesn't exist"; fi
+
+## usage
+
+```gci-start``` - start an instance
+```gci-connect``` - connect to an instance in a terminal
+```gci-stop``` - stop an instance
+```gci-get-ip``` - if you want to check what your current external IP is
+
+
+## Libraries
+### gci.sh
+- start stop and connect to an instance for ssh
+- will backup your previous ~/ssh/config to ~/.ssh/config.backup_UnixTimeSinceEpoch
---
+++ gci.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+export GCI_INSTANCE='instance-name'
+export GCI_EXTERNAL_IP="*"
+
+# 1. start (this will write ssh config)
+# 2. connect (will connect to the current instance)
+# 3. stop  (this will write ssh config)
+
+function gci-get-ip(){
+    gcloud compute instances list --filter="name=( 'NAME' ${GCI_INSTANCE} )" | awk '{print $5}' | tail -1
+}
+
+function gci-ssh-config-write-external-ip(){
+    ## writes the following to ssh config so that anything 
+    ## like VSCode that's searching for configured ssh hosts
+    ## will autodetect it - so you can just connect without
+    ## checking for gcloud IPs without making them static
+    backup_time=$(date +%s)
+    cp ~/.ssh/config ~/.ssh/config.backup_${backup_time}
+    echo "HOST ${GCI_EXTERNAL_IP}" > ~/.ssh/config
+    echo "    IdentityFile ~/.ssh/google_compute_engine" >> ~/.ssh/config
+    echo "    User kausthubk" >> ~/.ssh/config 
+    echo "    AddKeysToAgent yes" >> ~/.ssh/config
+    echo "    UseKeychain yes" >> ~/.ssh/config
+    echo "    ForwardAgent yes" >> ~/.ssh/config
+}
+
+function gci-start(){
+    gcloud compute instances start ${GCI_INSTANCE}
+    OUT=$(gci-get-ip)
+    export GCI_EXTERNAL_IP="${OUT}"
+    gci-ssh-config-write-external-ip
+}
+
+function gci-connect(){
+    gcloud beta compute ssh --ssh-flag="-A" "${GCI_INSTANCE}" -- -L 8888:localhost:8888
+}
+
+function gci-stop(){
+    gcloud compute instances stop ${GCI_INSTANCE}
+    export GCI_EXTERNAL_IP="*"
+    gci-ssh-config-write-external-ip
+}
